32ab8b691d9e1c0cf31c9bc9be6807ca7a4c9b70
Jack <98250548+jakcw@users.noreply.github.com>
2022-04-18 12:48:40 +1000

    Add files via upload

---
+++ HRIS/Database/DBAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace HRIS.Database
+{
+    public class DBAdapter
+    {
+        private const string db = "hris";
+        private const string user = "kit206g14a";
+        private const string pass = "group14a";
+        private const string server = "alacritas.cis.utas.edu.au";
+
+        private MySqlConnection conn;
+
+        public DBAdapter()
+        {
+            string connectionString = String.Format("Database={0};Data Source={1};User Id={2};Password={3}", db, server, user, pass);
+            conn = new MySqlConnection(connectionString);
+        }
+
+    }
+}
