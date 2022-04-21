f350fa70358b8fd9be87ea60823da7d13374bf17
Jack <98250548+jakcw@users.noreply.github.com>
2022-04-21 17:18:15 +1000

    Add files via upload

---
+++ HRIS/Controller/StaffController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using HRIS.Database;
+using HRIS.Entities;
+
+namespace HRIS.Controller
+{
+    class StaffController
+    {
+        public List<Staff> Staff { get; set; }
+        public ObservableCollection<Staff> ViewableStaff { get; set; }
+
+        public StaffController()
+        {
+            Staff = DBAdapter.GetStaffDetails();
+            ViewableStaff = new ObservableCollection<Staff>(Staff);
+        }
+
+        public ObservableCollection<Staff> GetViewableList()
+        {
+            return ViewableStaff;
+        }
+
+
+    }
+}
---
+++ HRIS/Controller/UnitController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using HRIS.Entities;
+using HRIS.Database;
+
+namespace HRIS.Controller
+{
+    class UnitController
+    {
+        public List<Unit> UnitList { get; set; }
+        public ObservableCollection<Unit> ViewableUnits { get; set; }                                           
+
+        public UnitController()
+        {
+            UnitList = DBAdapter.GetUnitDetails(123456);
+            ViewableUnits = new ObservableCollection<Unit>(UnitList);
+        }
+
+        public ObservableCollection<Unit> GetViewableUnits()
+        {
+            return ViewableUnits;
+        }
+       
+    }
+}
